32ddcf19afddd77d0e4b7f66be4a86829b24aac7
jellaan <jahn3@alaska.edu>
2025-08-19 17:34:21 +0200

    feat(navigation): implement a navigation between two screens

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,11 +15,10 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
-
-
   );
 }
 
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,17 +3,20 @@ import { FlatList } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>;
-}
-
-function CategoriesScreen() {
+function CategoriesScreen({ navigation }) {
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview');
+        }
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPressBtn={pressHandler}/>;
+    }
+    
     return (
         <FlatList 
             data={CATEGORIES} 
             keyExtractor={(item) => item.id} 
             renderItem={renderCategoryItem} 
-            numColumns={2 }
+            numColumns={2}
         />
     );
 }
