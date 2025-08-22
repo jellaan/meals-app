65fc2b3fc11343bcf047cc07671f490a6aa73bcc
jellaan <jahn3@alaska.edu>
2025-08-22 11:47:45 +0200

    feat(redux): use Redux state in a React component (FavoritesScreen)

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,17 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import { View, Text, StyleSheet } from "react-native";
+import { useSelector } from 'react-redux';
 
 import { MEALS } from '../data/dummy-data';
 import MealsList from '../components/MealsList/MealsList';
-import { FavoritesContext } from '../store/context/favorites-context';
+// import { FavoritesContext } from '../store/context/favorites-context';
 
 function FavoritesScreen() {
-    const favoriteMealsCtx = useContext(FavoritesContext);
+    // const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
     
-    const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+    // const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+    const favoriteMeals = MEALS.filter(meal => favoriteMealIds.includes(meal.id));
 
     if(favoriteMeals.length === 0) {
         return (
