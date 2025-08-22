c93f4244835b3d285f0afa89d386af11f9c0a8d3
jellaan <jahn3@alaska.edu>
2025-08-22 11:18:46 +0200

    feat(redux): create a Redux state slice and add the slice reducers to the store.

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import favoritesReducer from "./favorites";
+
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        favoriteMeals: favoritesReducer
+    }
 });
-
-
 
 // export default configureStore({
 //     reducer: {}
